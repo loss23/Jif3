4f3f9e564f389fd0c640fe45a7e8d49bd89da78f
loss23 <61843498+loss23@users.noreply.github.com>
2022-07-23 18:40:00 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,7 @@
 if game.PlaceId == 347401822 then
+	
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Rate1", "GrapeTheme")
     
     local NotificationBindable = Instance.new("BindableFunction")
 NotificationBindable.OnInvoke = callback
